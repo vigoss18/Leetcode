d29e037503a24d16f8873fcd5e10d37aceb9f50b
vigoss18 <vigoss18plus@gmail.com>
2017-05-06 21:04:01 +0800

    new AC

---
+++ 020. Valid Parentheses/Solution.h
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i = 0;i < s.size();i++)
+        {
+            if(st.empty())
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if((s[i] == ')' && st.top() == '(') ||
+                    (s[i] == ']' && st.top() == '[') ||
+                    (s[i] == '}' && st.top() == '{'))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(s[i]);
+                }
+            }
+        }
+        return st.empty();
+    }
+};
