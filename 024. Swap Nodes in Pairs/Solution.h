28c0dbda1f95bc75c6d66906d6175bb3d6ebd338
vigoss18 <vigoss18plus@gmail.com>
2017-05-17 22:54:00 +0800

    new AC

--- 024. Swap Nodes in Pairs/Solution.h
+++ 024. Swap Nodes in Pairs/Solution.h
@@ -7,35 +7,13 @@
  * };
  */
 class Solution {
-    struct cmp{
-        bool operator ()(const ListNode *a,const ListNode *b)
-        {
-            return a->val > b->val;
-        }
-    };
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,cmp>q;
-        for(int i = 0;i < lists.size();i++)
-        {
-            if(lists[i])
-                q.push(lists[i]);
-        }
-        if(q.empty())
-            return NULL;
-        ListNode *ret = q.top();
-        q.pop();
-        ListNode *tmp = ret;
-        if(ret->next)
-            q.push(ret->next);
-        while(!q.empty())
-        {
-            tmp->next = q.top();
-            tmp = tmp->next;
-            q.pop();
-            if(tmp->next)
-                q.push(tmp->next);
-        }
-        return ret;
+    ListNode* swapPairs(ListNode* head) {
+        if(!head) return NULL;
+        else if(!head->next) return head;
+        ListNode *tmp = head->next;
+        head->next = swapPairs(tmp->next);
+        tmp->next = head;
+        return tmp;
     }
 };
