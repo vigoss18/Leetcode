375a8f70098d7754fe2c498cf63946c0d100d056
vigoss18 <vigoss18plus@gmail.com>
2017-05-19 22:29:36 +0800

    new AC

--- 026. Remove Duplicates from Sorted Array/Solution.h
+++ 026. Remove Duplicates from Sorted Array/Solution.h
@@ -1,40 +1,15 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
-    struct cmp{
-        bool operator ()(const ListNode *a,const ListNode *b)
-        {
-            return a->val > b->val;
-        }
-    };
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,cmp>q;
-        for(int i = 0;i < lists.size();i++)
+    int removeDuplicates(vector<int>& nums) {
+        int ret = 0;
+        for(int i = 0;i < nums.size();i++)
         {
-            if(lists[i])
-                q.push(lists[i]);
-        }
-        if(q.empty())
-            return NULL;
-        ListNode *ret = q.top();
-        q.pop();
-        ListNode *tmp = ret;
-        if(ret->next)
-            q.push(ret->next);
-        while(!q.empty())
-        {
-            tmp->next = q.top();
-            tmp = tmp->next;
-            q.pop();
-            if(tmp->next)
-                q.push(tmp->next);
+            if(i == 0)
+                ret++;
+            else if(nums[i] != nums[i - 1])
+                ret++;
+            else
+                nums.erase(nums.begin() + i),i--;
         }
         return ret;
     }
